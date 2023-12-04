112036cf42438cb2586603418b2ec7259667cc7b
bmwrnnr88 <138243329+bmwrnnr88@users.noreply.github.com>
2023-12-04 14:21:32 -0800

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,7 +1,7 @@
 import openai
 import streamlit as st
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 st.title("💬 Chatbot")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
